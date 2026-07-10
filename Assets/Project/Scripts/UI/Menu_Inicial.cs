c0aba33939aeb7e16635f8b3fe4989099d9234ec
DrMonus <marioruizhtn@gmail.com>
2026-07-10 11:37:00 -0600

    Update_StarButton
    Update on the star, button

--- Assets/Project/Scripts/UI/Menu_Inicial.cs
+++ Assets/Project/Scripts/UI/Menu_Inicial.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class Menu_Inicial : MonoBehaviour
 {
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Jugar (){
 
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Botón Jugar presionado");
+        SceneManager.LoadScene("Pause");
     }
 
 
